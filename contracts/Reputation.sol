3c82ac012066c13a7944746079895d3e7a7871de
Otaiki1 <otaikisadiq2804@gmail.com>
2023-05-04 22:25:13 +0100

    fix: contract errors

--- contracts/Reputation.sol
+++ contracts/Reputation.sol
@@ -2,6 +2,13 @@
 pragma solidity ^0.8.9;
 
 contract Reputation {
+    //add constructor to collect address of proposal contract when deploying
+    address immutable ProposalContractAddress;
+
+    constructor(address _proposalContractAddress) {
+        ProposalContractAddress = _proposalContractAddress;
+    }
+
     mapping(address => uint256) public reputationPoints;
 
     function addReputation(
@@ -13,7 +20,7 @@ contract Reputation {
 
     modifier onlyProposalContract() {
         require(
-            msg.sender == proposalContract,
+            msg.sender == ProposalContractAddress,
             "Only the Proposal Contract can call this function."
         );
         _;
