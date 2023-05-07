819479f80217a0e36d50621951d61a341cd8e6c1
Abdulsamad sadiq <otaikisadiq2804@gmail.com>
2023-05-07 21:20:39 +0100

    refac: update deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  // networks: {
+  //   hardhat: {},
+  //   mantle: {
+  //     url: "https://testnet.mantlechain.com",
+  //     accounts: { mnemonic: process.env.MNEMONIC },
+  //     gas: "auto",
+  //     gasPrice: "auto",
+  //   },
+  // },
+  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {},
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
     mantle: {
-      url: "https://testnet.mantlechain.com",
-      accounts: { mnemonic: process.env.MNEMONIC },
+      url: "https://rpc.testnet.mantle.xyz/",
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      saveDeployments: true,
+      chainId: 5001,
       gas: "auto",
       gasPrice: "auto",
     },
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,6 +2,10 @@
 const { ethers, upgrades } = require("hardhat");
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying contracts with the account:', deployer.address);
+
+
   const Token = await ethers.getContractFactory("Token");
   const Membership = await ethers.getContractFactory("Membership");
   const Voting = await ethers.getContractFactory("Voting");
